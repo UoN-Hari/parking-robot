00702ab463774ad6c34920120f5811421798da46
UoN-Hari <211106370@qq.com>
2023-01-24 18:13:20 +0800

    fix the controller and add scripts for dependency

---
+++ ubuntu2004noetic.sh
@@ -0,0 +1,4 @@
+sudo apt install -y ros-noetic-joint-state-controller \
+ros-noetic-effort-controllers ros-noetic-position-controllers \
+ros-noetic-joint-trajectory-controller ros-noetic-ros-control \
+ros-noetic-ros-controllers
