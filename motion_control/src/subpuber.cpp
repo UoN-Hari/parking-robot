35a95be7485d15b5bc16f3eef09782660339f0a4
BSSN <1580336037@qq.com>
2023-04-12 14:27:54 +0800

    Add linear offset test

--- motion_control/src/subpuber.cpp
+++ motion_control/src/subpuber.cpp
@@ -13,18 +13,47 @@ void SubPuber::MotionControlCallBack()
   linearCmdVel.linear.x = linearSpd;
   geometry_msgs::Twist angularCmdVel;
   angularCmdVel.angular.z = angularSpd;
-  while(1) {
+
+  /*
+  // while(1) {
   // for(int i = 0; i <= 3; i++) {
     startTime =ros::Time::now().toSec();
+
     topicPuber0.publish(linearCmdVel);
     while((ros::Time::now().toSec() - startTime - lengthTimeOffset) < lengthTime) {}
+    // ROS_INFO("Cmdvel: %lf", linearCmdVel.linear.x);
     lengthTimeOffset = ros::Time::now().toSec() - startTime - lengthTimeOffset - lengthTime;
     ROS_INFO("LengthTimeOffset: %lf", lengthTimeOffset);
 
+    // startTime =ros::Time::now().toSec();
+    // topicPuber0.publish(angularCmdVel);
+    // while((ros::Time::now().toSec() - startTime + angularTimeOffset) < angularTime) {}
+    // angularTimeOffset = ros::Time::now().toSec() - startTime + angularTimeOffset - angularTime;
+    // ROS_INFO("AngularTimeOffset: %lf", angularTimeOffset);
+  // }
+  */
+
+
     startTime =ros::Time::now().toSec();
-    topicPuber0.publish(angularCmdVel);
-    while((ros::Time::now().toSec() - startTime + angularTimeOffset) < angularTime) {}
-    angularTimeOffset = ros::Time::now().toSec() - startTime + angularTimeOffset - angularTime;
-    ROS_INFO("AngularTimeOffset: %lf", angularTimeOffset);
-  }
+
+    // while((ros::Time::now().toSec() - startTime - lengthTimeOffset) < lengthTime) {
+      // topicPuber0.publish(linearCmdVel);
+    // }
+
+    ROS_INFO("Cmdvel: %lf", linearCmdVel.linear.x);
+    while(ros::Time::now().toSec() - startTime < 6) {
+      topicPuber0.publish(linearCmdVel); 
+    }
+    linearCmdVel.linear.x = 0;
+    topicPuber0.publish(linearCmdVel);
+    
+
+    lengthTimeOffset = ros::Time::now().toSec() - startTime - lengthTimeOffset - lengthTime;
+    ROS_INFO("LengthTimeOffset: %lf", lengthTimeOffset);
+
+    // startTime =ros::Time::now().toSec();
+    // topicPuber0.publish(angularCmdVel);
+    // while((ros::Time::now().toSec() - startTime + angularTimeOffset) < angularTime) {}
+    // angularTimeOffset = ros::Time::now().toSec() - startTime + angularTimeOffset - angularTime;
+    // ROS_INFO("AngularTimeOffset: %lf", angularTimeOffset);
 }
--- movement_calculation/src/subpuber.cpp
+++ movement_calculation/src/subpuber.cpp
@@ -3,16 +3,22 @@
 void SubPuber::MovementCalculationCallback(const geometry_msgs::Twist &cmdVel)
 {
   mcu_serial::motor_msgs motorCmdVel;
-  motorCmdVel.angular1 = cmdVel.linear.x - cmdVel.angular.z;
-  motorCmdVel.angular2 = cmdVel.linear.x + cmdVel.angular.z;
+  double angularL = motorOffset * (cmdVel.linear.x - cmdVel.angular.z);
+  double angularR = motorOffset * (cmdVel.linear.x + cmdVel.angular.z);
+
+  if(angularL - (int)angularL != 0 ||
+     angularR - (int)angularR != 0 ) {
+      ros::Exception("MOTOR SPEED NOT INT!!!!!!!!!!!!!!");
+  }
+
+  motorCmdVel.angular2 = angularL;
+  motorCmdVel.angular1 = angularR;
+
   // Right wheel offset ratio refers to Left wheel
   motorCmdVel.angular2 *= lrOffset;
-  // Motor offset from motor cmd to unit
-  motorCmdVel.angular1 *= motorOffset;
-  motorCmdVel.angular2 *= motorOffset;
 
-  motorCmdVel.dir1 = motorCmdVel.angular1 > 0 ? 0 : 1;
-  motorCmdVel.dir2 = motorCmdVel.angular2 > 0 ? 0 : 1;
+  motorCmdVel.dir1 = motorCmdVel.angular1 >= 0 ? 1 : 0;
+  motorCmdVel.dir2 = motorCmdVel.angular2 >= 0 ? 1 : 0;
 
   topicPuber0.publish(motorCmdVel);
 }
